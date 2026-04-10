1eb01ea1f60b14ef2b817c832d2239d26e82856c
ControlCenter Dev <controlcenter@local.dev>
2026-04-10 15:47:27 -0500

    initial control center build

---
+++ app/src/main/java/com/controlcenter/app/MainActivity.java
@@ -0,0 +1,27 @@
+package com.controlcenter.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView w = new WebView(this);
+
+        WebSettings s = w.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+
+        w.setWebViewClient(new WebViewClient());
+
+        w.loadUrl("file:///android_asset/index.html");
+
+        setContentView(w);
+    }
+}
